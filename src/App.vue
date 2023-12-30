87a3fbadd9c646d8389f24e20441aa84154dd6e3
Anuj Jhawar <anujjhawar28@gmail.com>
2023-12-30 05:41:00 +0530

    Skeleton Loader Component added.

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <!-- Example 1: Dynamic Width Loader -->
+    <SkeletonLoader />
+
+    <!-- Example 2: Circle/Avatar Loader with Different Sizes -->
+    <div style="display: flex; align-items: center">
+      <SkeletonLoader pill width="10%" tag="h2" />
+      <SkeletonLoader circle :size="50" tag="div" />
+      <SkeletonLoader circle :size="70" />
+    </div>
+
+    <!-- Example 2.1: Circle/Avatar Loader with Different Sizes -->
+    <SkeletonLoader preview />
+    <SkeletonLoader pill preview width="10%" tag="h2" />
+    <SkeletonLoader circle preview :size="50" />
+    <SkeletonLoader circle preview :size="70" />
+
+    <!-- Example 3: Color Background of Loader Change -->
+    <SkeletonLoader :backgroundColor="'#f0f0f0'" />
+    <SkeletonLoader :backgroundColor="'#a0a0a0'" />
+
+    <!-- Example 4: Table Listing Loader -->
+    <table>
+      <thead>
+        <tr>
+          <th>Column 1</th>
+          <th>Column 2</th>
+          <th>Column 3</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in tableData" :key="item.id">
+          <td>
+            <SkeletonLoader :width="100" />
+          </td>
+          <td>
+            <SkeletonLoader :width="150" />
+          </td>
+          <td>
+            <SkeletonLoader :width="80" />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Column 1</th>
+          <th>Column 2</th>
+          <th>Column 3</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in tableData" :key="item.id">
+          <td>
+            <SkeletonLoader preview :width="100" />
+          </td>
+          <td>
+            <SkeletonLoader preview :width="150" />
+          </td>
+          <td>
+            <SkeletonLoader preview :width="80" />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script lang="ts">
+import SkeletonLoader from "./components/SkeletonLoader.vue";
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  components: {
+    SkeletonLoader,
+  },
+  setup() {
+    const tableData = ref(
+      Array.from({ length: 5 }, (_, index) => ({ id: index }))
+    );
+
+    return {
+      tableData,
+    };
+  },
+});
+</script>
---
+++ src/components/SkeletonLoader.vue
@@ -0,0 +1,144 @@
+<template>
+  <Component
+    :is="tag"
+    class="skeleton-box"
+    :style="style"
+    :class="{ preview }"
+  />
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, toRefs } from "vue";
+
+export default defineComponent({
+  name: "SkeletonLoader",
+  props: {
+    width: {
+      type: [Number, String],
+      default: "100%",
+    },
+    height: {
+      type: [Number, String],
+      default: "32px",
+    },
+    size: {
+      type: [Number, String],
+    },
+    circle: {
+      type: Boolean,
+      default: false,
+    },
+    pill: {
+      type: Boolean,
+      default: false,
+    },
+    preview: {
+      type: Boolean,
+      default: false,
+    },
+    backgroundColor: {
+      type: String,
+      default: "#dddbdd",
+    },
+    duration: {
+      type: String,
+      default: "1.5s",
+    },
+    gradient: {
+      type: String,
+      default:
+        "linear-gradient(90deg, rgba(255, 255, 255, 0) 0, rgba(255, 255, 255, 0.2) 20%, rgba(255, 255, 255, 0.5) 60%, rgba(255, 255, 255, 0))",
+    },
+    animationTimingFunction: {
+      type: String,
+      default: "ease-in-out",
+    },
+    tag: {
+      type: String,
+      default: "span",
+      required: true,
+    },
+  },
+  setup(props) {
+    const { width, height, size, pill, circle } = toRefs(props);
+    const convertUnits = (
+      str: string | number | null | undefined,
+      unit = "px"
+    ): string | undefined => {
+      if (str == null || str === "") {
+        return undefined;
+      } else if (isNaN(str as number)) {
+        return String(str);
+      } else {
+        return `${Number(str)}${unit}`;
+      }
+    };
+
+    const style = computed(() => {
+      let _style = {
+        width: convertUnits(width.value),
+        height: convertUnits(height.value),
+      };
+      if (circle.value || size.value) {
+        _style = {
+          width: convertUnits(size.value),
+          height: convertUnits(size.value),
+        };
+      }
+      return _style;
+    });
+
+    const borderRadius = computed(() => {
+      if (pill.value && height.value && width.value) {
+        return "9999px";
+      } else if (circle.value && size.value) {
+        return "50%";
+      } else {
+        return "";
+      }
+    });
+
+    const displayClass = computed(() => {
+      if (!circle.value && size.value) {
+        return "display-block";
+      } else {
+        return "display-inline";
+      }
+    });
+
+    return {
+      borderRadius,
+      style,
+      displayClass,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.skeleton-box {
+  display: inherit;
+  position: relative;
+  overflow: hidden;
+  background-color: v-bind(backgroundColor);
+  border-radius: v-bind(borderRadius);
+}
+@keyframes Shimmer {
+  100% {
+    transform: translateX(100%);
+  }
+}
+.skeleton-box:not(.preview):after {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  transform: translateX(-100%);
+  background: v-bind(gradient);
+  animation: Shimmer v-bind(duration) infinite v-bind(animationTimingFunction);
+  content: "";
+}
+</style>
