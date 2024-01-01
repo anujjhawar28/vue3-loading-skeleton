a83ca3137cd14e2186345b74e397969fb638c535
Anuj Jhawar <anujjhawar28@gmail.com>
2024-01-02 01:52:55 +0530

    Usage in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
   <div>
     <!-- Example 1: Dynamic Width Loader -->
+    <h3>Default Skeleton Loader</h3>
     <SkeletonLoader />
 
+    <h3>Default Skeleton Loader with size property only</h3>
+    <SkeletonLoader size="100" />
+
     <!-- Example 2: Circle/Avatar Loader with Different Sizes -->
+    <h3>Skeleton Loader with Circle and size property and custom Tags</h3>
     <div style="display: flex; align-items: center">
-      <SkeletonLoader pill width="10%" tag="h2" />
+      <SkeletonLoader pill width="10%" tag="h3" />
       <SkeletonLoader circle :size="50" tag="div" />
-      <SkeletonLoader :size="70" />
+      <SkeletonLoader circle :size="70" />
     </div>
 
     <!-- Example 2.1: Circle/Avatar Loader with Different Sizes -->
+    <h3>Preview mode of the Components</h3>
     <SkeletonLoader preview />
-    <SkeletonLoader pill preview width="10%" tag="h2" />
+    <SkeletonLoader pill preview width="10%" tag="h3" />
     <SkeletonLoader circle preview :size="50" />
-    <SkeletonLoader circle preview :size="70" />
 
     <!-- Example 3: Color Background of Loader Change -->
-    <SkeletonLoader :backgroundColor="'#f0f0f0'" />
+    <h3>Change Background Color of Component</h3>
+    <SkeletonLoader :backgroundColor="'#343541'" />
     <SkeletonLoader :backgroundColor="'#a0a0a0'" />
+    <SkeletonLoader :backgroundColor="'red'" />
 
     <!-- Example 4: Table Listing Loader -->
+    <h3>Table Representation</h3>
     <table>
       <thead>
         <tr>
@@ -44,6 +52,7 @@
       </tbody>
     </table>
 
+    <h3>Table Representation Preview</h3>
     <table>
       <thead>
         <tr>
